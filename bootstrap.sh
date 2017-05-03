0e7aa552ece91067f4278a737bdda6ae6c2c9d98
Yann Ramin <yann@theatr.us>
2017-05-03 04:01:03 +0000

    Bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,9 @@ GMP_VERSION=6.1.2
 MPFR_VERSION=3.1.5
 MPC_VERSION=1.0.3
 
+export CC=arm-none-eabi-gcc
+export CFLAGS="-nostartfiles --specs=nosys.specs -mcpu=cortex-m4 -Os -mfloat-abi=hard -mfpu=fpv4-sp-d16"
 
-rm -rf buildtmp
 rm -rf extlib
 
 mkdir -p buildtmp
@@ -19,28 +20,45 @@ EXTLIB=$PWD/extlib
 
 pushd .
 cd buildtmp
-wget https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.bz2
-wget http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.gz
-wget http://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
-wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-${GCC_VERSION}-linux.tar.bz2
+if [ ! -f gmp-${GMP_VERSION}.tar.bz2 ]; then
+    wget https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.bz2
+fi
 
-tar xvjf gcc-arm-none-eabi-${GCC_VERSION}-linux.tar.bz2
-tar xvjf gmp-${GMP_VERSION}.tar.bz2
-tar xvzf mpfr-${MPFR_VERSION}.tar.gz
-tar xvzf mpc-${MPC_VERSION}.tar.gz
+if [ ! -f mpfr-${MPFR_VERSION}.tar.gz ]; then
+    wget http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.gz
+fi
+
+if [ ! -f mpc-${MPC_VERSION}.tar.gz ]; then
+    wget http://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
+fi
+
+if [ ! -f gcc-arm-none-eabi-${GCC_VERSION}-linux.tar.bz2 ]; then
+    wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-${GCC_VERSION}-linux.tar.bz2
+fi
+
+if [ ! -d gcc-arm-none-eabi-${GCC_VERSION} ]; then
+    rm -rf gcc-arm-none-eabi-${GCC_VERSION}
+    tar xjf gcc-arm-none-eabi-${GCC_VERSION}-linux.tar.bz2
+fi
+
+rm -rf gmp-${GMP_VERSION}
+tar xjf gmp-${GMP_VERSION}.tar.bz2
+rm -rf mpfr-${MPFR_VERSIO}
+tar xzf mpfr-${MPFR_VERSION}.tar.gz
+tar xzf mpc-${MPC_VERSION}.tar.gz
 
 export PATH=$PATH:$PWD/gcc-arm-none-eabi-${GCC_VERSION}/bin
 
 pushd .
 cd gmp-${GMP_VERSION}
-./configure CC=arm-none-eabi-gcc CFLAGS="-I$HOME/gmprot/include -nostartfiles --specs=nosys.specs -mcpu=cortex-m4 -Os" LDFLAGS="-L$HOME/gmprot" --host=arm-none-eabi --disable-assembly --prefix=$EXTLIB --disable-shared
+./configure  --host=arm-none-eabi --disable-assembly --prefix=$EXTLIB --disable-shared
 make -j 4
 make install
 popd
 
 pushd .
 cd mpfr-${MPFR_VERSION}
-./configure CC=arm-none-eabi-gcc CFLAGS="-I$HOME/gmprot/include -nostartfiles --specs=nosys.specs -mcpu=cortex-m4 -Os" LDFLAGS="-L$HOME/gmprot" --host=arm-none-eabi --disable-assembly --prefix=$EXTLIB --disable-shared --with-gmp=${EXTLIB}
+./configure --host=arm-none-eabi --disable-assembly --prefix=$EXTLIB --disable-shared --with-gmp=${EXTLIB}
 make -j 4
 make install
 popd
